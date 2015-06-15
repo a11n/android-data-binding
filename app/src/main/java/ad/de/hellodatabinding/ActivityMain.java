a7908186dc52dafb2b496e239aabd47772604ed0
a11n <andre.diermann@gmail.com>
2015-06-15 17:07:51 +0200

    Refactored activity name

--- app/src/main/java/ad/de/hellodatabinding/ActivityMain.java
+++ app/src/main/java/ad/de/hellodatabinding/ActivityMain.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
