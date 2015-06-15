c958f35b500e41a8e4cf1e2cd061f60a7d252abd
a11n <andre.diermann@gmail.com>
2015-06-15 22:07:38 +0200

    Simplified code even more.

--- app/src/main/java/ad/de/hellodatabinding/ActivityMain.java
+++ app/src/main/java/ad/de/hellodatabinding/ActivityMain.java
@@ -13,7 +13,6 @@ import android.widget.EditText;
 public class ActivityMain extends AppCompatActivity {
 
   private ActivityMainBinding binding;
-  private ViewModelMain model;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -26,29 +25,7 @@ public class ActivityMain extends AppCompatActivity {
   @Override protected void onResume() {
     super.onResume();
 
-    model = new ViewModelMain("World");
-
-    binding.setModel(model);
-  }
-
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
+    binding.setName("World");
   }
 
   private class MyTextWatcher implements TextWatcher {
@@ -57,7 +34,7 @@ public class ActivityMain extends AppCompatActivity {
     }
 
     @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
-      model.name.set(s.toString());
+      binding.setName(s.toString());
     }
 
     @Override public void afterTextChanged(Editable s) {
